4b5f4b10f5ff8251e692a0f037f6d60efc4601e9
Graham King <gking2224@gmail.com>
2016-10-13 12:41:32 +0100

    tidying

--- src/main/java/me/gking2224/common/utils/RandomString.java
+++ src/main/java/me/gking2224/common/utils/RandomString.java
@@ -1,9 +1,7 @@
 package me.gking2224.common.utils;
 
 import java.math.BigInteger;
-import java.nio.ByteBuffer;
 import java.security.SecureRandom;
-import java.util.Arrays;
 import java.util.Base64;
 
 public class RandomString {
